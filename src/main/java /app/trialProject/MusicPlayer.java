9727527e84ba9169c5a0b7c6ea1b25dfa49c5be0
Ivan <ripidipin@gmail.com>
2025-08-25 18:53:28 +0300

    setter usege test now for value

--- src/main/java /app/trialProject/MusicPlayer.java
+++ src/main/java /app/trialProject/MusicPlayer.java
@@ -2,6 +2,8 @@ package app.trialProject;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private Integer volume;
 
     public MusicPlayer(Music music) {
         this.music = music;
@@ -10,11 +12,32 @@ public class MusicPlayer {
     public MusicPlayer() {}
 
     public void play() {
-        System.out.println("Playing music " + music.getSong());
+        System.out.println("Playing music " + music.getSong() + " from " + name +
+                "\nwith volume " + volume);
     }
 
     public void setMusic(Music music) {
         this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
     }
 }
 
